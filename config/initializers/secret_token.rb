218d4d24deeda1b233246158196c075973554bf5
Mac-os <mac-os@MacBook-de-Mac-os.local>
2012-12-07 10:20:12 +0100

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExampleApp::Application.config.secret_token = 'b5f754ee1d2c5bfdcd65c406ea4091ce26ec1f18986b3b5a9238456c80ab0a3a20b9f01ea5071fc1f255cced13eab19cbc0488ee15706259ab7251019e8dbfda'
